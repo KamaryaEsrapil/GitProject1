e82f3346b43d61e3005899a0cd700972df6ee752
KamaryaEsrapil <138307647+KamaryaEsrapil@users.noreply.github.com>
2023-08-10 09:52:39 +0800

    our sixth commit

--- src/day1/Animal.java
+++ src/day1/Animal.java
@@ -3,5 +3,9 @@ package day1;
 public class Animal {
     public static void main(String[] args) {
         System.out.println("hello guys");
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i);;
+
+        }
     }
 }
