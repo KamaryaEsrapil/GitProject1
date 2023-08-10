1a4ede0e5520ea5fc76b7a163c5e9426633779db
KamaryaEsrapil <138307647+KamaryaEsrapil@users.noreply.github.com>
2023-08-10 08:43:20 +0800

    our scecond change

--- src/day1/Student.java
+++ src/day1/Student.java
@@ -1,4 +1,10 @@
 package day1;
 
 public class Student {
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i);
+
+        }
+    }
 }
