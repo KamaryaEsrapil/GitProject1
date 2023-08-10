c45f12a5179d5d95bd7ef6dd0be45ee554b58919
KamaryaEsrapil <138307647+KamaryaEsrapil@users.noreply.github.com>
2023-08-10 15:02:37 +0800

    first commit

--- src/day1test/githuptest.java
+++ src/day1test/githuptest.java
@@ -4,6 +4,7 @@ public class githuptest {
     public static void main(String[] args) {
         for (int i = 0; i < 10; i++) {
             System.out.println(i);
+            int n = 29;
 
         }
     }
